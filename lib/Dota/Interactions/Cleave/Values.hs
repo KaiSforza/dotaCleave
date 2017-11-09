36ff6dce3c80b62751325faaf4c2f31889dfcd83
William Giokas <1007380@gmail.com>
2017-11-09 13:45:14 -0700

    Generalize the `cleave` function, add 7.07
    It no longer requires a trapezoid and circular cleave, but can take one
    of each or two of the same. Anything that implements Cleave.
    
    I also added in changes for 7.07. Because Battle Fury has changed
    multiple times, I also made the `bfuryTrap` set up values, including
    values for the trapezoidal/conical cleave in 7.0, 7.02 and 7.02
    separately. the `bfury` value uses the newest one paried with the old
    circular cleave.
    
    There has also been some new output. With the generalized `cleave`
    function, I added comparisons between patches (7.00->7.02, 7.02-7.07).
    
    Note that this does mean that the type changed, and requires you to
    use `uncurry` with old values, as well as removed the `Maybe` from the
    second argument in `cleave`. You can just use the `standardCleave` value
    there, as it has an area of 0.

--- lib/Dota/Interactions/Cleave/Values.hs
+++ lib/Dota/Interactions/Cleave/Values.hs
@@ -3,29 +3,40 @@ module Dota.Interactions.Cleave.Values where
 import Dota.Interactions.Cleave.Types
 
 -- http://dota2.gamepedia.com/Battle_Fury
+-- 7.07 bfury
+bfury :: (TrapezoidCleave, CircleCleave)
+bfury = ( bfuryTrap''
+        , CircleCleave {radius=280})
+
+-- 7.07
+bfuryTrap'' :: TrapezoidCleave
+bfuryTrap'' = standardCleave {end=330, distance=625}
+-- 7.02 bfury
+bfuryTrap' :: TrapezoidCleave
+bfuryTrap' = standardCleave {end=300, distance=570}
+
 -- 7.00-7.01 bfury: standardCleave {end=280, distance=520}
-bfury :: (TrapezoidCleave, Maybe CircleCleave)
-bfury = ( standardCleave {end=300, distance=570}
-        , Just CircleCleave {radius=280})
+bfuryTrap :: TrapezoidCleave
+bfuryTrap = standardCleave {end=280, distance=520}
 
 -- http://dota2.gamepedia.com/Kunkka
-kunkka :: (TrapezoidCleave, Maybe CircleCleave)
+kunkka :: (TrapezoidCleave, CircleCleave)
 -- 7.00 -7.01 cleave: standardCleave {end=600, distance=900}
 kunkka = ( standardCleave {end=650, distance=1100}
-         , Just CircleCleave {radius=600})
+         , CircleCleave {radius=600})
 
 -- http://dota2.gamepedia.com/Magnus
-magnus :: (TrapezoidCleave, Maybe CircleCleave)
+magnus :: (TrapezoidCleave, CircleCleave)
 magnus = ( standardCleave {end=240, distance=460}
-         , Just CircleCleave {radius=240})
+         , CircleCleave {radius=240})
 
 -- http://dota2.gamepedia.com/Sven
-sven :: (TrapezoidCleave, Maybe CircleCleave)
+sven :: (TrapezoidCleave, CircleCleave)
 sven = ( standardCleave {end=300, distance=550}
-       , Just CircleCleave {radius=300})
+       , CircleCleave {radius=300})
 
 -- http://dota2.gamepedia.com/Tiny
-tiny :: (TrapezoidCleave, Maybe CircleCleave)
+tiny :: (TrapezoidCleave, CircleCleave)
 tiny = ( standardCleave {end=400, distance=600}
-       , Just CircleCleave {radius=400})
+       , CircleCleave {radius=400})
 
--- src/Main.hs
+++ src/Main.hs
@@ -6,32 +6,36 @@ import Text.Printf
 import Dota.Interactions.Cleave.Types
 import Dota.Interactions.Cleave.Values
 
-cleave :: (TrapezoidCleave, Maybe CircleCleave) -> (Float, Maybe Float, Maybe Float)
-cleave (new, old) = (newArea, originalArea, ratio)
+cleave :: (Cleave t, Cleave c) => t -> c -> (Float, Float, Float)
+cleave new old = (newArea, originalArea, ratio)
     where newArea = cleaveArea new
-          originalArea = fmap cleaveArea old
-          ratio = fmap (newArea /) originalArea
+          originalArea = cleaveArea old
+          ratio = newArea / originalArea
 
 -- These numbers are not rounded, just truncated.
-formatReddit :: (PrintfArg t, Floating t) => String -> (t, Maybe t, Maybe t) -> String
-formatReddit name (newArea, Just originalArea, Just ratio) =
+formatReddit :: (PrintfArg t, Floating t) => String -> (t, t, t) -> String
+formatReddit name (newArea, originalArea, ratio) =
     printf "* %s: `%.f` (~%.1f%%) (original area: ~`%.f`)" name newArea (ratio * 100) originalArea
 
--- In case we don't have an original cleave (new items)
-formatReddit name (newArea, _, _) = printf "* %s: `%.f`" name newArea
+-- -- In case we don't have an original cleave (new items)
+-- formatReddit name (newArea, _, _) = printf "* %s: `%.f`" name newArea
 
 main :: IO ()
 main = putStr $ unlines
-    [ formatReddit "Battlefury" $ cleave bfury
-    , formatReddit "Empower" $ cleave magnus
-    , formatReddit "Grow with Aghs" $ cleave tiny
-    , formatReddit "Tidebringer" $ cleave kunkka
-    , formatReddit "Great Cleave" $ cleave sven
+    [ formatReddit "Battlefury" $ uncurry cleave bfury
+    , formatReddit "Bfury 7.06->7.07" $ cleave bfuryTrap bfuryTrap'
+    , formatReddit "Bfury 7.01->7.02" $ cleave bfuryTrap' bfuryTrap''
+    , formatReddit "Empower" $ uncurry cleave magnus
+    , formatReddit "Grow with Aghs" $ uncurry cleave tiny
+    , formatReddit "Tidebringer" $ uncurry cleave kunkka
+    , formatReddit "Great Cleave" $ uncurry cleave sven
     ]
 
 -- Output:
--- * Battlefury: `223600` (~90.8%) (original area: ~`246301`)
+-- * Battlefury: `300000` (~121.8%) (original area: ~`246301`)
+-- * Bfury 7.06->7.07: `300000` (~117.0%) (original area: ~`256500`)
+-- * Bfury 7.01->7.02: `256500` (~114.7%) (original area: ~`223600`)
 -- * Empower: `179400` (~99.1%) (original area: ~`180956`)
 -- * Grow with Aghs: `330000` (~65.7%) (original area: ~`502655`)
--- * Tidebringer: `675000` (~59.7%) (original area: ~`1130973`)
+-- * Tidebringer: `880000` (~77.8%) (original area: ~`1130973`)
 -- * Great Cleave: `247500` (~87.5%) (original area: ~`282743`)
